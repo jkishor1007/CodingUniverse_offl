093f9eebc637e3dcd08ce2904018ac57fe8d56d6
Mr Black Panther <84783693+jkishor1007@users.noreply.github.com>
2022-01-25 22:43:31 +0530

    Add files via upload
    This is The File For Python Starters!
    You Should Fill Everything in this File,
    You Can Use Many Editors Like, 
    PyCharm
    VS Code
    Sublime Text
    Notepad++
    And Some Online Compliers

---
+++ Begin Python.py
@@ -0,0 +1,16 @@
+#Fill This All!
+
+#Variables
+num1 = ("") #Enter a Number
+num2 = ("") #Enter a Number
+
+name1 = ("") #Enter a Value
+name2 = ("") #Enter a Value
+#Lists
+your choice  = [""] #Enter Some Values
+your choice  = [""] #Enter Some Values
+#Main
+#Start Your Main Code Here
+#E.g. print("num1")
+    # print("name1")
+    # print("list(-1)")
