fc8e1b9d9b69be4bb0a707c10afd5f8d774ce6ec
Mr Black Panther <84783693+jkishor1007@users.noreply.github.com>
2022-01-27 12:54:02 +0530

    Add files via upload

---
+++ Start Python.py
@@ -0,0 +1,37 @@
+#Variables
+#IF Ur Not Active in Any Of The Below, Put N/A
+
+my_name = ("Enter Your Name")
+my_age = ("Enter Your Age")
+my_dob = ("Enter Your DOB")
+my_mail = ("mail id: ")
+my_discord_id = ("Discord: ")
+my_instagram_id = ("Instagram: ")
+my_valorant_id = ("Valorant: ")
+my_minecraft_ign = ("Minecraft: ")
+my_roblox_id = ("Roblox: ")
+my_fortnite_id = ("Fortnite: ")
+
+#Lists
+
+my_friends = ["0" , "1" , "2" , "3" , "4" , "5"]
+
+#Main Code
+
+print("My Name is " + my_name + ", ")
+print("My Age is " +my_age+ ", ")
+print("I was Born on " +my_dob+ ", ")
+print(f'My friends are {my_friends}.')
+print("Contact Me: Mail Id= " +my_mail+ ",")
+print("Discord Id= " +my_discord_id+ ", ")
+print("Instagram Id= " +my_instagram_id+ ". ")
+
+print("My IGNs: Valorant= " +my_valorant_id+ ", " )
+print("Minecraft= " +my_minecraft_ign+ ", ")
+print("Roblox= " +my_roblox_id+ ", ")
+print("Fortnite= " +my_fortnite_id+ ". ")
+print("Basically I'm Very Happy To Learn Python!")
+
+
+
+
